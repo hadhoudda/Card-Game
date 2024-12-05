96c3f1a956af01c5a6a92de958c84b2968bd7289
Houda Ben Abdallah <ahhouda@yahoo.fr>
2024-12-05 22:32:52 +0100

    first commit

---
+++ src/com/houda/cardgame/model/PlayingCard.java
@@ -0,0 +1,30 @@
+package com.houda.cardgame.model;
+
+public class PlayingCard {
+
+    private Rank rank;
+    private Suit suit;
+    private boolean faceUp;
+
+    public PlayingCard(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public boolean isFaceUp(){
+        return faceUp;
+    }
+
+    public boolean flip(){
+        faceUp = !faceUp;
+        return faceUp;
+    }
+}
